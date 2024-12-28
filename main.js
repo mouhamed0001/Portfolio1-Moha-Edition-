c6f1bca28bb61450228ea521f5da6b0826458e06
3mimr <omarsoudanig03@gmail.com>
2024-12-28 13:12:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".var-text" ,{
+    strings: ["Student" ,"Porgrammer","Web Devlopment"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
+var typed2 = new Typed(".var-text2" ,{
+    strings: ["Student..." ,"Porgrammer...","Web Devlopment..."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
