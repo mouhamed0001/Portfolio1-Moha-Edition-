9a36b07a73f70d612f96884f49967c0fe8090c6b
mouhamed0001 <tanjirobouraib003@gmail.com>
2024-12-29 10:50:48 +0100

    simple changes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var typed = new Typed(".var-text" ,{
-    strings: ["Student" ,"Porgrammer","Web Devlopment"],
+    strings: ["Student" ,"Porgrammer","Web Devloper"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true,
 })
 var typed2 = new Typed(".var-text2" ,{
-    strings: ["Student..." ,"Porgrammer...","Web Devlopment..."],
+    strings: ["Student..." ,"Porgrammer...","Web Devloper..."],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
